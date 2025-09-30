4928b5ffb3a2f40fcea6d2d2c6a62db788f67da2
Sadaf Jamil <sadaf220099@gmail.com>
2025-09-30 04:02:14 -0700

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createBrowserRouter ,RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from "./pages/home"
 const router =createBrowserRouter (
   [
     {
